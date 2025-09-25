5d3666a67e899709e088f54ff958f7e1eaebd576
MichaelCai0301 <michaelcai0301@gmail.com>
2025-09-25 19:41:51 -0400

    combined and filtered media info; everything is in scifisubgenres or altered_data

--- scifisubgenres/scifi_filter.py
+++ scifisubgenres/scifi_filter.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Create all_scifi_genres.csv by concatenating all CSV files in the scifi_genres directory
 # ! NOTE: only run ONCE! Ty :)
-all_scifi_exists = True
+all_scifi_exists = False
 if not all_scifi_exists:
     input_folder = os.path.join(os.path.dirname(__file__), 'scifi_genres')
     output_file = os.path.join(os.path.dirname(__file__), 'all_scifi_genres.csv')
@@ -19,6 +19,8 @@ if not all_scifi_exists:
 
 # Filter out non-scifi genres based on all_scifi_genres.csv from a given csv
 def filter_scifi_genres(input_csv, output_csv):
+    input_test_file = os.path.join(os.path.dirname(__file__), input_csv)
+    output_test_file = os.path.join(os.path.dirname(__file__), output_csv)
     all_scifi_file = os.path.join(os.path.dirname(__file__), 'all_scifi_genres.csv')
     all_scifi_df = pd.read_csv(all_scifi_file)
     all_scifi_genres = set(all_scifi_df['title'].str.lower().tolist())
@@ -26,11 +28,13 @@ def filter_scifi_genres(input_csv, output_csv):
     input_df = pd.read_csv(input_csv)
     filtered_df = input_df[input_df['title'].str.lower().isin(all_scifi_genres)]
     filtered_df.to_csv(output_csv, index=False)
+    print("Filtered")
 
 # Test: (it works!)
-test_scifi_filter = True
+test_scifi_filter = False
 if test_scifi_filter:
-    input_test_file = os.path.join(os.path.dirname(__file__), 'test.csv')
-    output_test_file = os.path.join(os.path.dirname(__file__), 'test_filtered.csv')
-    filter_scifi_genres(input_test_file, output_test_file)
+    filter_scifi_genres('test.csv', 'test_filtered.csv')
     print("Test filtering complete. Check 'test_filtered.csv'.")
+
+
+
